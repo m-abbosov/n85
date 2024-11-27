16b3d602105ee7e076a80905833f46e0b06caee3
Muhammadamin <m.abbbosov@gmail.com>
2024-11-27 17:59:44 +0500

    javascript kirish

---
+++ 4-month/javascript/app.js
@@ -0,0 +1,28 @@
+// let A = 20
+// let B = 3;
+
+// console.log("A + B =", A + B); // 23
+// console.log(A - B); //17
+// console.log(A * B); //60
+// console.log(A / B); //6.6667
+// console.log(A ** B); // 8000
+// console.log(A % B); // 2
+
+
+// let C = +prompt("Son kiriting!")
+
+
+// C++;
+
+// console.log(C); // 11
+// C = C + 1
+// console.log(C); //12
+// C += son
+// C=C+son
+// console.log(C); // 13
+
+// alert("Hello")
+
+
+console.log("5");
+console.log(5);
