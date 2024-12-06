99f7748fc84aaa4efd0622176d70dd7ecaf38bdb
Muhammadamin <m.abbbosov@gmail.com>
2024-12-06 16:11:50 +0500

    if and switch operator

---
+++ 4-month/conditional/app.js
@@ -0,0 +1,65 @@
+// const num = 50.235323;
+// console.log(num.toFixed(2));
+// let num = 555;
+
+// console.log(num + "");
+
+
+// const a = 4;
+// const b = "10";
+
+
+// console.log(a + b) // 410
+// console.log(a - b) // -6
+// console.log(a * b) // 10
+// console.log(a / b) // 4
+
+// const num = "a"
+
+// console.log(isNaN(NaN));
+
+
+// const a = ''
+// if (Boolean(a)) {
+//     console.log("ha");
+// } else {
+//     console.log("yo'q");
+
+// }
+
+// console.log(55 && 20 && null && 40 && 30); // false
+// console.log(undefined || "" || 0 || NaN || null); // true
+// console.loug(55 && 0);
+// console.log(55 || 0);
+// console.log(0 && 55);
+// console.log(NaN ?? 55);
+// console.log(55 || 0);
+
+// let a = 25;
+// let b = 20;
+// console.log(a > b ? a + b : b - a);
+
+
+// console.log(max);
+
+
+
+// if (5 > 3 && 5 > 10) {
+//     console.log("a");
+// } else if (5 > 3 || 5 > 10) {
+//     console.log("b");
+// } else {
+//     console.log("c");
+// }
+
+const pul = "dekabr";
+
+switch (pul) {
+    case "dekabr":
+    case "yanvar":
+    case "fevral": console.log("qish"); break;
+    case 400:
+    case 500:
+    case 600: console.log("iphone"); break;
+    default: console.log("Telefon omaymiz");
+}
