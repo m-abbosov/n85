9b1fbc62a2dfd45612d96b368798934e1dad478e
Muhammadamin <m.abbbosov@gmail.com>
2024-12-09 17:59:37 +0500

    functions

---
+++ 4-month/functions/app.js
@@ -0,0 +1,86 @@
+// sayHello()
+
+// function sayHello() {
+//     console.log("Hello");
+//     console.log(Math.random());
+// }
+
+// function add(son1, son2) {
+//     console.log(son1 + son2);
+// }
+
+// add(2, 5)
+// add(10, 5)
+
+// function helloFullName(firstName, lastName) {
+//     var fullName = firstName + " " + lastName;
+//     var res = "Hello " + fullName + " !"
+//     console.log(res);
+// }
+
+
+
+// function square(a = 1, b = 1) {
+//     return a * b;
+
+
+
+//     console.log("aaa");
+// }
+
+// const tortburchak1 = square(8) // 120 // 80
+// const tortburchak2 = square(7, 10)
+
+// console.log(tortburchak1); // 40
+// console.log(tortburchak2); // 70
+
+
+
+// function fullName(firstName = "None", job = "ishsiz") {
+//     if (job !== "ishsiz") {
+//         return "Hello, " + firstName + " " + job + "!";
+//     }
+
+//     return "Hello " + firstName + "!"
+// }
+
+
+// console.log(fullName("Azamat"));
+
+
+// function getValue() {
+//     alert("Hello")
+// }
+
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// let sum = add;
+
+// // let result = add(10, 20);
+
+// let result = sum(10, 20);
+
+
+// console.log(result);
+
+
+// const add = function (a, b) {
+//     return a + b
+// }
+
+// console.log(add(4, 20));
+
+// console.log(add(4, 20));
+
+// const add = a => a * a
+
+
+// if (6 < 10) console.log(add(2));
+
+
+(function (a, b) {
+    console.log(a + b);
+})(20, 50)
