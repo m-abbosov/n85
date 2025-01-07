76dd49f6918dba15bc661a2e362c12f83c75412d
Muhammadamin <m.abbbosov@gmail.com>
2025-01-07 16:02:11 +0500

    debugging

---
+++ 5-month/debugging/app.js
@@ -0,0 +1,88 @@
+// "use strict"
+// let a = 10;
+
+// delete a;
+
+
+
+// var a = 20;
+
+// function test() {
+//     var a = 100;
+//     console.log(a);
+// }
+
+// test()
+
+
+// console.log(eval(222 + 222));
+
+
+// function test() {
+//     test()
+// }
+
+// test()
+
+
+// try {
+//     console.log(a);
+// } catch (error) {
+//     // throw new Error("a o'zgaruvchi aniqlanmagan")
+//     console.error("Xatolik")
+// } finally {
+//     console.log("Bu yerda yozilgan kod har doim try yoki catch bajarilgan keyin ishlaydi");
+// }
+
+// g = 20;
+// console.log(g);
+
+
+// function ab() {
+//     "use strict"
+//     a = 20;
+//     console.log(a);
+// }
+
+// ab()
+
+
+// console.log("aa");
+
+
+// console.log(a);
+
+
+// var a = 10;
+
+
+// let gSon = 20;
+
+// const tashqiFunksiya = () => {
+//     console.log(this);
+// }
+
+// tashqiFunksiya()
+
+// const person = {
+//     firstName: "Aziz",
+//     lastName: "Berdiyev",
+//     fullName: () => {
+//         console.log(this)
+//     }
+// }
+
+// person.fullName()
+
+
+// function sayHello(ism, age) {
+//     console.log(`Salom, ${ism}. Sizning yoshingiz taxminan ${age}. Mening ismim ${this.name}`);
+// }
+
+// const user = {
+//     name: "Javohir"
+// }
+
+// sayHello.bind(user, "Nodir", 23)()
+
+// newFunction()
