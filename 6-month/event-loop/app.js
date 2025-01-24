19d20fd3896b7dbcb401038d9686578d063bf66e
Muhammadamin <m.abbbosov@gmail.com>
2025-01-24 20:07:46 +0500

    event loop

---
+++ 6-month/event-loop/app.js
@@ -0,0 +1,17 @@
+console.log(1);
+console.log(2);
+
+setTimeout(function () {
+    console.log("Hello");
+}, 10000)
+
+setTimeout(function () {
+    console.log("Hi");
+}, 5000)
+
+setTimeout(function () {
+    console.log("BYE");
+}, 5000)
+
+console.log(3);
+console.log(4);
